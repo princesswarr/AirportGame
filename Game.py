5b7091dc7c761dbf7335cb9b41fc5c29b8064980
princesswarr <frigate289@gmail.com>
2022-10-03 10:45:23 +0300

    Intro v.0

--- Game.py
+++ Game.py
@@ -1,1 +1,28 @@
-print("Hello World!")
+import time, os, importlib
+#
+version_of_the_game = "Airport Game v.0"
+
+# Intro the the Game + Tutorial
+
+print(f"Hello and Welcome to {version_of_the_game}")
+time.sleep(0.8)
+print("Do you want to play a new game (1) or the tutorial (2) ?")
+time.sleep(1.3)
+choice_1 = int(input("Enter: "))
+os.system('cls')
+while choice_1 == 2:
+    Tutorial = importlib.import_module('Tutorial')
+    time.sleep(1)
+    print("Are you ready to play the game? (yes / no) ")
+    time.sleep(1)
+    ready_to_play = input("Enter: ")
+    if ready_to_play == "no":
+        os.system('cls')
+        importlib.reload(Tutorial)
+        continue
+    else:
+        break
+
+# The Actual Game
+
+print("The Game")
