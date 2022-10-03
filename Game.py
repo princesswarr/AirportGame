90ad1a9583dea56b9a0e28225baeef09135363cd
princesswarr <frigate289@gmail.com>
2022-10-03 13:21:36 +0300

    v.0.1

--- Game.py
+++ Game.py
@@ -1,6 +1,6 @@
 import time, os, importlib
 #
-version_of_the_game = "Airport Game v.0"
+version_of_the_game = "Airport Game v.0.1"
 
 # Intro the the Game + Tutorial
 
@@ -25,4 +25,33 @@ while choice_1 == 2:
 
 # The Actual Game
 
-print("The Game")
+def pkgtype():
+    type = random.randint(1, 3)
+    if type == 1:
+        type = "Silver"
+    elif type == 2:
+        type = "Bronze"
+    elif type == 3:
+        type = "Gold  "
+    return type
+
+
+player_location = "Helsinki-Vantaa Airport"
+first_airport = 1
+second_airport = 2
+third_airport = 3
+fourth_airport = 4
+fifth_airport = 5
+co2_left = difficulty
+score = 350
+print(f"You are in: {player_location}                 "
+      f"CO2 left: {co2_left}                 "
+      f"Score: {score} "
+      f"\nChoose next location: "
+      f"\n      Type        Airport                     Distance"
+      f"\n1.    {pkgtype()}     {first_airport}"
+      f"\n2.    {pkgtype()}     {second_airport}"
+      f"\n3.    {pkgtype()}     {third_airport}"
+      f"\n4.    {pkgtype()}     {fourth_airport}"
+      f"\n5.    {pkgtype()}     {fifth_airport}")
+Choice = int(input("Enter: "))
--- Tutorial.py
+++ Tutorial.py
@@ -1,1 +1,39 @@
-import Game
+##
+import random
+def new_turn():
+        def function():
+            def pkgtype():
+                type = random.randint(1, 3)
+                if type == 1:
+                    type = "Silver"
+                elif type == 2:
+                    type = "Bronze"
+                elif type == 3:
+                    type = "Gold  "
+                return type
+            # RANDOM AIRPORT
+            random_airport = "bob"
+            airport_pkg = [f"{pkgtype()}", f"{random_airport}"]
+            return airport_pkg
+
+        player_location = "Helsinki-Vantaa Airport"
+        airport_pkg1 = function()
+        airport_pkg2 = function()
+        airport_pkg3 = function()
+        airport_pkg4 = function()
+        airport_pkg5 = function()
+        co2_left = 10000
+        score = 350
+        print(f"You are in: {player_location}                 "
+              f"CO2 left: {co2_left}                 "
+              f"Score: {score} "
+              f"\nChoose next location: "
+              f"\n      Type        Airport                     Distance"
+              f"\n1.    {airport_pkg1[0]}     {airport_pkg1[1]}"
+              f"\n2.    {airport_pkg2[0]}     {airport_pkg1[1]}"
+              f"\n3.    {airport_pkg3[0]}     {airport_pkg1[1]}"
+              f"\n4.    {airport_pkg4[0]}     {airport_pkg1[1]}"
+              f"\n5.    {airport_pkg5[0]}     {airport_pkg1[1]}")
+        Choice = int(input("Enter: "))
+
+new_turn()
