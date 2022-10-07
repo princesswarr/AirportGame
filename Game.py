8387e4b799203ddee727adacdf3a763c6cf2dcf8
princesswarr <frigate289@gmail.com>
2022-10-07 11:33:52 +0300

    v.0.3

--- Game.py
+++ Game.py
@@ -1,7 +1,15 @@
-import time, os, importlib, random
-#
+import time, os, importlib, random, mysql.connector
+from geopy import distance
+connection = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='root',
+    password='1234',
+    autocommit=True
+)
 os.system('cls')
-version_of_the_game = "Airport Game v.0.2"
+version_of_the_game = "Airport Game v.0.3"
 
 # Intro the the Game + Tutorial
 
@@ -50,34 +58,247 @@ elif dif_choice == 3:
     print("\nChosen difficulty = Hard")
 time.sleep(0.8)
 os.system('cls')
-#Game
-def pkgtype():
-    type = random.randint(1, 3)
-    if type == 1:
-        type = "Silver"
-    elif type == 2:
-        type = "Bronze"
-    elif type == 3:
-        type = "Gold  "
-    return type
 
 
-player_location = "Helsinki-Vantaa Airport"
-first_airport = 1
-second_airport = 2
-third_airport = 3
-fourth_airport = 4
-fifth_airport = 5
-co2_left = difficulty
-score = 350
-print(f"You are in: {player_location}                 "
-      f"CO2 left: {co2_left}                 "
-      f"Score: {score} "
-      f"\nChoose next location: "
-      f"\n      Type        Airport                     Distance"
-      f"\n1.    {pkgtype()}     {first_airport}"
-      f"\n2.    {pkgtype()}     {second_airport}"
-      f"\n3.    {pkgtype()}     {third_airport}"
-      f"\n4.    {pkgtype()}     {fourth_airport}"
-      f"\n5.    {pkgtype()}     {fifth_airport}")
-Choice = int(input("Enter: "))
+
+#Game - Main Function
+def new_turn(new_location, coleft, current_score):
+    #FUNCTIONS
+
+
+    def function():
+        def pkgtype():
+            type = random.randint(1, 3)
+            if type == 1:
+                type = "Silver"
+            elif type == 2:
+                type = "Bronze"
+            elif type == 3:
+                type = "Gold  "
+            return type
+
+        # RANDOM AIRPORT
+        random_airport = "bob"
+        dist = 5
+        airport_pkg = [f"{pkgtype()}", f"{random_airport}", f"{dist}"]
+        return airport_pkg
+
+    ### WEATHER & POINT CALCULATION
+
+    def weather (type):
+        if type == 1:
+            print("Points gained: 100 ")
+            time.sleep(1)
+            chance_of_rain = random.randint(1,100)
+            randomstring = random.randint (1,2)
+            weather_effect = 0
+            if chance_of_rain <= 2:
+                weather_effect = 10
+                if randomstring == 1:
+                    print("Oh NO! An unlikely burst of rain has slashed your points to 90")
+                    time.sleep(2)
+                if randomstring == 2:
+                    print("Oh shoot... Extreme sunshine has slashed your points to 90")
+                    time.sleep(2)
+            amount_left_points = 100 - weather_effect
+
+        elif type == 2:
+            print("Points gained: 200 ")
+            time.sleep(1)
+            chance_of_rain = random.randint(1, 100)
+            randomstring = random.randint(1, 2)
+            weather_effect = 0
+            if chance_of_rain <= 2:
+                weather_effect = 10
+                if randomstring == 1:
+                    print("Oh NO! An unlikely burst of rain has slashed your points to 190")
+                    time.sleep(2)
+                if randomstring == 2:
+                    print("Oh shoot... Extreme sunshine has slashed your points to 190")
+                    time.sleep(2)
+
+            if chance_of_rain<25 and chance_of_rain >=3:
+                weather_effect = 110
+                if randomstring == 1:
+                    print("Oh NO! A rare windy storm has reduced your points to 90")
+                    time.sleep(2)
+                if randomstring == 2:
+                    print("Oh man! A lightning storm has cut your points to 90")
+                    time.sleep(2)
+            amount_left_points = 200 - weather_effect
+        elif type == 3:
+            print("Points gained: 300 ")
+            time.sleep(1)
+            chance_of_rain = random.randint(1, 100)
+            randomstring = random.randint(1, 2)
+            weather_effect = 0
+            if chance_of_rain <= 2:
+                weather_effect = 20
+                if randomstring == 1:
+                    print("Oh NO! An unlikely burst of rain has slashed your points to 280")
+                    time.sleep(2)
+                if randomstring == 2:
+                    print("Oh shoot... Extreme sunshine has slashed your points to 280")
+                    time.sleep(2)
+
+            if chance_of_rain < 25 and chance_of_rain >= 3:
+                weather_effect = 210
+                if randomstring == 1:
+                    print("Oh NO! A loud thunder has reduced your points to 90")
+                    time.sleep(2)
+                if randomstring == 2:
+                    print("Oh man! A TORNADO has cut your points to 90")
+                    time.sleep(2)
+            if chance_of_rain < 55 and chance_of_rain >= 25:
+                weather_effect = 130
+                if randomstring == 1:
+                    print("Oh NO! A loud thunder has reduced your points to 90")
+                    time.sleep(2)
+                if randomstring == 2:
+                    print("Oh man! A TORNADO has cut your points to 90")
+                    time.sleep(2)
+            amount_left_points = 300 - weather_effect
+
+        return amount_left_points
+
+    player_location = new_location
+
+    airport_pkg1 = function()
+    airport_pkg1.append(1)
+
+    airport_pkg2 = function()
+    airport_pkg2.append(2)
+
+    airport_pkg3 = function()
+    airport_pkg3.append(3)
+
+    airport_pkg4 = function()
+    airport_pkg4.append(4)
+
+    airport_pkg5 = function()
+    airport_pkg5.append(5)
+
+    co2_left = coleft
+    score = current_score
+    print(f"You are in: {player_location}                 "
+          f"CO2 left: {co2_left}                 "
+          f"Score: {score} "
+          f"\nChoose next location: "
+          f"\n      Type        Airport                  Distance (CO2)"
+          f"\n{airport_pkg1[3]}.    {airport_pkg1[0]}      {airport_pkg1[1]}                      {airport_pkg1[2]}"
+          f"\n{airport_pkg2[3]}.    {airport_pkg2[0]}      {airport_pkg2[1]}                      {airport_pkg2[2]}"
+          f"\n{airport_pkg3[3]}.    {airport_pkg3[0]}      {airport_pkg3[1]}                      {airport_pkg3[2]}"
+          f"\n{airport_pkg4[3]}.    {airport_pkg4[0]}      {airport_pkg4[1]}                      {airport_pkg4[2]}"
+          f"\n{airport_pkg5[3]}.    {airport_pkg5[0]}      {airport_pkg5[1]}                      {airport_pkg5[2]}")
+    Choice = int(input("Enter: "))
+
+
+
+    if Choice == 1:
+        distance_to = int(airport_pkg1[2])
+        co2_left = co2_left - distance_to
+        airport_pkg1.append(co2_left)
+
+
+        if airport_pkg1[0] == "Bronze":
+            amount_points = weather(1)
+            time.sleep(2)
+        elif airport_pkg1[0] == "Silver":
+            amount_points = weather(2)
+            time.sleep(2)
+        elif airport_pkg1[0] == "Gold  ":
+            amount_points = weather(3)
+            time.sleep(2)
+        score = score + amount_points
+        airport_pkg1.append(score)
+        returnlist = airport_pkg1
+
+    if Choice == 2:
+        distance_to = int(airport_pkg2[2])
+        co2_left = co2_left - distance_to
+        airport_pkg2.append(co2_left)
+
+
+        if airport_pkg2[0] == "Bronze":
+            amount_points = weather(1)
+            time.sleep(2)
+        elif airport_pkg2[0] == "Silver":
+            amount_points = weather(2)
+            time.sleep(2)
+        elif airport_pkg2[0] == "Gold  ":
+            amount_points = weather(3)
+            time.sleep(2)
+        score = score + amount_points
+        airport_pkg2.append(score)
+        returnlist = airport_pkg2
+
+    if Choice == 3:
+        distance_to = int(airport_pkg3[2])
+        co2_left = co2_left - distance_to
+        airport_pkg3.append(co2_left)
+
+
+        if airport_pkg3[0] == "Bronze":
+            amount_points = weather(1)
+            time.sleep(2)
+        elif airport_pkg3[0] == "Silver":
+            amount_points = weather(2)
+            time.sleep(2)
+        elif airport_pkg3[0] == "Gold  ":
+            amount_points = weather(3)
+            time.sleep(2)
+        score = score + amount_points
+        airport_pkg3.append(score)
+        returnlist = airport_pkg3
+    if Choice == 4:
+        distance_to = int(airport_pkg4[2])
+        co2_left = co2_left - distance_to
+        airport_pkg4.append(co2_left)
+
+
+        if airport_pkg4[0] == "Bronze":
+            amount_points = weather(1)
+            time.sleep(2)
+        elif airport_pkg4[0] == "Silver":
+            amount_points = weather(2)
+            time.sleep(2)
+        elif airport_pkg4[0] == "Gold  ":
+            amount_points = weather(3)
+            time.sleep(2)
+        score = score + amount_points
+        airport_pkg4.append(score)
+        returnlist = airport_pkg4
+
+    if Choice == 5:
+        distance_to = int(airport_pkg5[2])
+        co2_left = co2_left - distance_to
+        airport_pkg5.append(co2_left)
+
+
+        if airport_pkg5[0] == "Bronze":
+            amount_points = weather(1)
+            time.sleep(2)
+        elif airport_pkg5[0] == "Silver":
+            amount_points = weather(2)
+            time.sleep(2)
+        elif airport_pkg5[0] == "Gold  ":
+            amount_points = weather(3)
+            time.sleep(2)
+        score = score + amount_points
+        airport_pkg5.append(score)
+        returnlist = airport_pkg5
+
+    return returnlist
+starting_score = 0
+starting_location = "helsinki"
+starting_difficulty = difficulty
+
+returnls = new_turn(starting_location,starting_difficulty,starting_score)
+for x in range (10):
+    os.system('cls')
+    returnls = new_turn(returnls[1],returnls[4],returnls[5])
+
+os.system('cls')
+print("\n\n\n\n\nGame Over\n\n")
+print(f"Your score was: {returnls[5]}")
+###
--- Tutorial.py
+++ Tutorial.py
@@ -22,7 +22,7 @@ os.system('cls')
 
 print("\n\nTutorial\n")
 time.sleep(1)
-print("In this game you play as a postal pilot collecting packages from different airports in Finland.\n"
+print("In this game you play as a postal pilot collecting packages from different airports.\n"
       "You start from a random airport and get given a choice of five airports to travel to.\n"
       "\n")
 tutorialinput = input("Press enter to continue: ")
@@ -86,39 +86,3 @@ print(f"You are in: Helsinki-Vantaa Airport                 "
 tutorialinput = input("Press enter to continue: ")
 if tutorialinput == "":
     time.sleep(0.2)
-def new_turn():
-        def function():
-            def pkgtype():
-                type = random.randint(1, 3)
-                if type == 1:
-                    type = "Silver"
-                elif type == 2:
-                    type = "Bronze"
-                elif type == 3:
-                    type = "Gold  "
-                return type
-            # RANDOM AIRPORT
-            random_airport = "bob"
-            airport_pkg = [f"{pkgtype()}", f"{random_airport}"]
-            return airport_pkg
-
-        player_location = "Helsinki-Vantaa Airport"
-        airport_pkg1 = function()
-        airport_pkg2 = function()
-        airport_pkg3 = function()
-        airport_pkg4 = function()
-        airport_pkg5 = function()
-        co2_left = 10000
-        score = 350
-        print(f"You are in: {player_location}                 "
-              f"CO2 left: {co2_left}                 "
-              f"Score: {score} "
-              f"\nChoose next location: "
-              f"\n      Type        Airport                     Distance (CO2)"
-              f"\n1.    {airport_pkg1[0]}     {airport_pkg1[1]}"
-              f"\n2.    {airport_pkg2[0]}     {airport_pkg2[1]}"
-              f"\n3.    {airport_pkg3[0]}     {airport_pkg3[1]}"
-              f"\n4.    {airport_pkg4[0]}     {airport_pkg4[1]}"
-              f"\n5.    {airport_pkg5[0]}     {airport_pkg5[1]}")
-        Choice = int(input("Enter: "))
-
